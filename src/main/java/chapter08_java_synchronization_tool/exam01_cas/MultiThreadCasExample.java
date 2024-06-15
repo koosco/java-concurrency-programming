2f82a93816069f2a20c34f6df636a630ab685658
koosco <rnxogud123@naver.com>
2024-06-15 17:20:46 +0900

    chapter09: cas 예제

---
+++ src/main/java/chapter08_java_synchronization_tool/exam01_cas/MultiThreadCasExample.java
@@ -0,0 +1,39 @@
+package chapter08_java_synchronization_tool.exam01_cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThreadCasExample {
+    private static AtomicInteger value = new AtomicInteger(0);
+    private static int THREAD_COUNT = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 100_000; j++) {
+                    int expectedValue;
+                    int newValue;
+                    do {
+                        expectedValue = value.get();
+                        newValue = expectedValue + 1;
+                    } while (!value.compareAndSet(expectedValue, newValue)); // 값이 다르다면 재시도를 수행
+//                    value = newValue;
+
+                    System.out.println(Thread.currentThread().getName() + ": " + expectedValue + ": " + newValue);
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i].join();
+        }
+
+        System.out.println("최종값 = " + value.get());
+    }
+}
+/**
+ * cas는 lock이나 synchronized를 이용한 mutex를 이용하는 것이 아니라 cpu 원자성을 이용해 현재값과 기댓값을 비교하는 방법이다
+ * <p>
+ * 굉장히 많은 쓰레드가 실패를 반복한다면 성능이 안좋을 수 있음 -> 잠깐 대기했다고 돌아가면 됨 cas연산이 많아지게 되면 성능이 감소하게 됨
+ */
---
+++ src/main/java/chapter08_java_synchronization_tool/exam01_cas/NonAtomicExample.java
@@ -0,0 +1,28 @@
+package chapter08_java_synchronization_tool.exam01_cas;
+
+public class NonAtomicExample {
+
+    private static int value = 0;
+    private static int THREAD_COUNT = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 100_000; j++) {
+                    int expectedValue = value; // value를 임시적으로 저장
+                    int newValue = expectedValue + 1; // 연산을 수행
+                    value = newValue;
+                    System.out.println(Thread.currentThread().getName() + ": " + expectedValue + ": " + newValue);
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i].join();
+        }
+
+        System.out.println("최종값 = " + value);
+    }
+}
