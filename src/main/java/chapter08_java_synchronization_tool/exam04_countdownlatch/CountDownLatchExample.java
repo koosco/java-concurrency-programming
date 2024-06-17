65596746f6d0264d8eae696a07c34b7705a709e7
koosco <rnxogud123@naver.com>
2024-06-18 00:50:39 +0900

    chapter09: countdownlatch 예제

---
+++ src/main/java/chapter08_java_synchronization_tool/exam04_countdownlatch/CountDownLatchExample.java
@@ -0,0 +1,45 @@
+package chapter08_java_synchronization_tool.exam04_countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchExample {
+    public static void main(String[] args) throws InterruptedException {
+        int numThreads = 5;
+        CountDownLatch startSignal = new CountDownLatch(1);
+        CountDownLatch doneSignal = new CountDownLatch(5);
+
+        for (int i = 0; i < numThreads; i++) {
+            new Thread(new Worker(startSignal, doneSignal)).start();
+        }
+
+        Thread.sleep(3000);
+        startSignal.countDown();
+        System.out.println("시작 신호를 알렸습니다");
+        doneSignal.await();
+        System.out.println("모든 Thread가 작업을 완료했습니다");
+    }
+
+    static class Worker implements Runnable {
+        private final CountDownLatch startSignal;
+        private final CountDownLatch doneSignal;
+
+        public Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+            this.startSignal = startSignal;
+            this.doneSignal = doneSignal;
+        }
+
+        @Override
+        public void run() {
+            try {
+                startSignal.await(); // main thread에서 시작 신호를 알림
+                System.out.println(Thread.currentThread().getName() + " 가 작업을 수행하고 있습니다");
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " 가 작업을 완료했습니다");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }finally {
+                doneSignal.countDown();
+            }
+        }
+    }
+}
